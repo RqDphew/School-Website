81d8a60a162a838f52b5689d76e99a64795b11f2
RqDphew <94170082+RqDphew@users.noreply.github.com>
2022-04-04 19:12:47 +0200

    Delete School-Website-main/school-website directory

--- School-Website-main/school-website/year_updater.js
+++
@@ -1,4 +0,0 @@
-const dt = new Date();
-let currentYear = parseInt(dt.getFullYear());
-let nextYear = currentYear+1;
-document.getElementById("yearDet").innerHTML="Упис за учебната "+currentYear.toString()+'/'+nextYear.toString();
