70290cf8a03101a42665155bc83834c5d7cbc6b7
Ce3Pi0 <75101073+Ce3Pi0@users.noreply.github.com>
2022-04-09 19:31:45 +0200

    Add files via upload

--- school-website/school-website/german/main/myscripts.js
+++ school-website/school-website/german/main/myscripts.js
@@ -165,7 +165,7 @@ function myFunction() {
 function change(lang){
     var path = final_path.substring(0, final_path.lastIndexOf("/"));
     var directoryName = path.substring(path.lastIndexOf("/")+1);
-    if(final_page=="elektro_docelektro_doc.html" || final_page == "mashinski_doc.html")
+    if(final_page=="elektro_doc.html" || final_page == "mashinski_doc.html")
         location.href="../../../../"+lang+"/struki/"+directoryName+"/"+final_page;   
     else
         location.href = "../../"+lang+"/"+directoryName+"/"+final_page;
