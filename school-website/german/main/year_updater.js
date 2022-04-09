d33feffaec58ed8831ba89f18c67a6c8181ae2ce
RqDphew <94170082+RqDphew@users.noreply.github.com>
2022-04-09 19:36:21 +0200

    Delete school-website/german directory

--- school-website/german/main/year_updater.js
+++
@@ -1,4 +0,0 @@
-const dt = new Date();
-let currentYear = parseInt(dt.getFullYear());
-let nextYear = currentYear+1;
-document.getElementById("yearDet").innerHTML="Einschreibung für das Studienjahr"+currentYear.toString()+'/'+nextYear.toString();
