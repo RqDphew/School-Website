a7cf969bc5824d0550b79cf0164059f5cfc4fd5f
Ce3Pi0 <75101073+Ce3Pi0@users.noreply.github.com>
2022-04-10 23:53:06 +0200

    Add files via upload

--- school-website/english/home/calc_code.js
+++ school-website/english/home/calc_code.js
@@ -2,19 +2,19 @@ function getVal() {
     let sumPovedenie = 0, sumMatematika = 0, sumMakedonski = 0, sumAngliski = 0, sumFizika = 0, totalSum = 0, natprevar = 0;
     for(let i=1;i<=4;i++){
         if(Number.isNaN(parseInt(document.getElementById('povedenie'+i).value))==true || parseInt(document.getElementById('povedenie'+i).value) > 5){
-            window.alert("Грешка");    
+            window.alert("Error");    
             return;
         }
         if(Number.isNaN(parseInt(document.getElementById('math'+i).value))==true || parseInt(document.getElementById('math'+i).value) > 5){
-            window.alert("Грешка");    
+            window.alert("Error");    
             return;
         }
         if(Number.isNaN(parseInt(document.getElementById('jazik'+i).value))==true || parseInt(document.getElementById('jazik'+i).value) > 5){
-            window.alert("Грешка");    
+            window.alert("Error");    
             return;
         }
         if(Number.isNaN(parseInt(document.getElementById('stranski'+i).value))==true || parseInt(document.getElementById('stranski'+i).value) > 5){
-            window.alert("Грешка");    
+            window.alert("Error");    
             return;
         }
         sumPovedenie+=parseInt(document.getElementById('povedenie'+i).value);
@@ -24,11 +24,11 @@ function getVal() {
     }
     
     if(Number.isNaN(parseInt(document.getElementById('fizika1').value))==true || parseInt(document.getElementById('fizika1').value) > 5){
-        window.alert("Грешка");    
+        window.alert("Error");    
         return;
     }
     if(Number.isNaN(parseInt(document.getElementById('fizika2').value))==true || parseInt(document.getElementById('fizika2').value) > 5){
-        window.alert("Грешка");    
+        window.alert("Error");    
         return;
     }
     sumFizika+=parseInt(document.getElementById('fizika1').value);
@@ -43,5 +43,5 @@ function getVal() {
     natprevar = parseInt(select.options[select.selectedIndex].value);
     totalSum+=Math.round(((sumMatematika+sumMakedonski+sumAngliski+sumFizika)*10)/4+sumMatematika+sumMakedonski+sumAngliski+sumFizika+sumPovedenie+natprevar);
 
-    document.getElementById("poeni").innerHTML="Поени за упис: "+totalSum;
+    document.getElementById("poeni").innerHTML="Points for enrollment: "+totalSum;
   }
