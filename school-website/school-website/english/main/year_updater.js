49c907da47297087bc024ec1ad1c8a84efcfb559
RqDphew <94170082+RqDphew@users.noreply.github.com>
2022-04-10 02:13:48 +0200

    Delete school-website/school-website directory

--- school-website/school-website/english/main/year_updater.js
+++
@@ -1,4 +0,0 @@
-const dt = new Date();
-let currentYear = parseInt(dt.getFullYear());
-let nextYear = currentYear+1;
-document.getElementById("yearDet").innerHTML="Enrollment for the academic year "+currentYear.toString()+'/'+nextYear.toString();
